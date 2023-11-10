3f4b3f2af28a91e5566efeb7c8cf75bd78d2be71
Mateusz Majewski <mat.maj1992@gmail.com>
2023-11-11 00:21:59 +0100

    Learning in progress

---
+++ dotnet-course-code/HelloWorld/Models/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HelloWorld
+{
+
+    public class Computer
+    {
+        //private string _motherboard;
+        public string Motherboard { get; set; } = "";
+        public int CPUCores { get; set; }
+        public bool HasWifi { get; set; }
+        public bool HasLTE { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+        public string VideoCard { get; set; } = "";
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer myComputer = new Computer()
+            {
+                Motherboard = "Z690",
+                HasWifi = true,
+                HasLTE = false,
+                ReleaseDate = DateTime.Now,
+                Price = 943.87m,
+                CPUCores = 8,
+                VideoCard = "RTX 2060"
+            };
+            myComputer.HasWifi = false;
+            Console.WriteLine(myComputer.Motherboard);
+            Console.WriteLine(myComputer.HasWifi);
+            Console.WriteLine(myComputer.ReleaseDate);
+        }
+    }
+}
