b08391679c4f2a80a95a4ae68d4c057fe29c55f1
Mateusz Majewski <mat.maj1992@gmail.com>
2023-11-12 00:42:51 +0100

    Learning in progress

--- dotnet-course-code/HelloWorld/Methods/Program.cs
+++ dotnet-course-code/HelloWorld/Methods/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace HelloWorld
+﻿using Models.Models;
+
+namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Computer computer = new Computer();
             int[] intsToCompress = new int[] { 10, 15, 20, 25, 30, 12, 34 };
 
             int totalValue = 0;
@@ -14,7 +17,20 @@
 
             Console.WriteLine(totalValue);
 
-
+            Computer myComputer = new Computer()
+            {
+                Motherboard = "Z690",
+                HasWifi = true,
+                HasLTE = false,
+                ReleaseDate = DateTime.Now,
+                Price = 943.87m,
+                CPUCores = 8,
+                VideoCard = "RTX 2060"
+            };
+            myComputer.HasWifi = false;
+            Console.WriteLine(myComputer.Motherboard);
+            Console.WriteLine(myComputer.HasWifi);
+            Console.WriteLine(myComputer.ReleaseDate);
         }
 
         static private int GetSum(int[] numbers, int sum)
--- dotnet-course-code/HelloWorld/Models/Program.cs
+++ dotnet-course-code/HelloWorld/Models/Program.cs
@@ -1,22 +1,26 @@
-﻿namespace HelloWorld
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Models.Models;
+using System.Data;
+
+namespace HelloWorld
 {
 
-    public class Computer
-    {
-        //private string _motherboard;
-        public string Motherboard { get; set; } = "";
-        public int CPUCores { get; set; }
-        public bool HasWifi { get; set; }
-        public bool HasLTE { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public decimal Price { get; set; }
-        public string VideoCard { get; set; } = "";
-
-    }
+   
     internal class Program
     {
         static void Main(string[] args)
         {
+            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;";
+
+            IDbConnection dbConnection = new SqlConnection(connectionString);
+
+            string sqlCommand = "SELECT GETDATE()";
+
+            DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+
+            Console.WriteLine(rightNow);
+
             Computer myComputer = new Computer()
             {
                 Motherboard = "Z690",
