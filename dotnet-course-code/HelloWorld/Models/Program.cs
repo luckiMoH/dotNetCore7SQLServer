991367b934858daaf2ff1fc71343211f2a092d98
Mateusz Majewski <mat.maj1992@gmail.com>
2023-11-14 17:40:27 +0100

    Learning in progress. Started to play with a SQL DataBase

--- dotnet-course-code/HelloWorld/Models/Program.cs
+++ dotnet-course-code/HelloWorld/Models/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Data.SqlClient;
 using Models.Models;
 using System.Data;
+using System.Globalization;
 
 namespace HelloWorld
 {
@@ -28,13 +29,61 @@ namespace HelloWorld
                 HasLTE = false,
                 ReleaseDate = DateTime.Now,
                 Price = 943.87m,
-                CPUCores = 8,
                 VideoCard = "RTX 2060"
             };
-            myComputer.HasWifi = false;
-            Console.WriteLine(myComputer.Motherboard);
-            Console.WriteLine(myComputer.HasWifi);
-            Console.WriteLine(myComputer.ReleaseDate);
+
+            Console.WriteLine(myComputer.Price); // comma jako separator
+            Console.WriteLine(myComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)); // zamiana comma na dot jako separator
+
+            string sql = @"INSERT INTO TutorialAppSchema.Computer (
+                Motherboard,
+                HasWifi,
+                HasLTE,
+                ReleaseDate,
+                Price,
+                VideoCard
+            ) VALUES ('" + myComputer.Motherboard 
+               + "','" + myComputer.HasWifi 
+               + "','" + myComputer.HasLTE 
+               + "','" + myComputer.ReleaseDate.ToString("yyyy-MM-dd")
+               + "','" + myComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)
+               + "','" + myComputer.VideoCard
+            + "')";
+
+            //Console.WriteLine(sql);
+
+            int result = dbConnection.Execute(sql); // dodawanie wartości do bazy danych na podstawie stworzonego SQL Query sql
+            //Console.WriteLine(result);
+
+            string sqlSelect = @"
+            SELECT 
+                Computer.Motherboard,
+                Computer.HasWifi,
+                Computer.HasLTE,
+                Computer.ReleaseDate,
+                Computer.Price,
+                Computer.VideoCard
+               FROM TutorialAppSchema.Computer";
+
+            IEnumerable<Computer> computers = dbConnection.Query<Computer>(sqlSelect);
+
+            Console.WriteLine("Tutaj są dane przesłane z bazy danych");
+
+            foreach (Computer singleComputer in computers)
+            {
+                Console.WriteLine("'" + singleComputer.Motherboard
+               + "','" + singleComputer.HasWifi
+               + "','" + singleComputer.HasLTE
+               + "','" + singleComputer.ReleaseDate.ToString("yyyy-MM-dd")
+               + "','" + singleComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)
+               + "','" + singleComputer.VideoCard
+            + "'");
+            }
+
+            //myComputer.HasWifi = false;
+            //Console.WriteLine(myComputer.Price);
+            //Console.WriteLine(myComputer.HasWifi);
+            //Console.WriteLine(myComputer.ReleaseDate);
         }
     }
 }
