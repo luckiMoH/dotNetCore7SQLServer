8cc751abe0405e26466f2024771413b14d55aee5
Mateusz Majewski <mat.maj1992@gmail.com>
2023-11-20 13:20:17 +0100

    Learning in progress. Auto mapper and JSON Properties

--- dotnet-course-code/HelloWorld/ReadAndWriteFiles/Models/Computer.cs
+++ dotnet-course-code/HelloWorld/ReadAndWriteFiles/Models/Computer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Models.Models
@@ -9,14 +10,21 @@ namespace Models.Models
     public class Computer
     {
         //private string _motherboard;
-
+        [JsonPropertyName("computer_id")]
         public int ComputerId { get; set; }
+        [JsonPropertyName("motherboard")]
         public string Motherboard { get; set; } = "";
+        [JsonPropertyName("cpu_cores")]
         public int? CPUCores { get; set; } = 0;
+        [JsonPropertyName("has_wifi")]
         public bool HasWifi { get; set; }
+        [JsonPropertyName("has_lte")]
         public bool HasLTE { get; set; }
+        [JsonPropertyName("release_date")]
         public DateTime? ReleaseDate { get; set; }
+        [JsonPropertyName("price")]
         public decimal Price { get; set; }
+        [JsonPropertyName("video_card")]
         public string VideoCard { get; set; } = "";
 
     }
--- dotnet-course-code/HelloWorld/ReadAndWriteFiles/Program.cs
+++ dotnet-course-code/HelloWorld/ReadAndWriteFiles/Program.cs
@@ -1,4 +1,5 @@
-﻿using Dapper;
+﻿using AutoMapper;
+using Dapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Models.Data;
@@ -57,52 +58,97 @@ namespace HelloWorld
 
             //openFile.Close();
 
-            string computersJson = File.ReadAllText("Computers.json"); // czytanie Computers.json file ktory jest w bin
+            //string computersJson = File.ReadAllText("Computers.json"); // czytanie Computers.json file ktory jest w bin
+            string computersJson = File.ReadAllText("ComputersSnake.json"); // czytanie Computers.json file ktory jest w bin
 
-            //Console.WriteLine(computersJson);
-
-            JsonSerializerOptions options = new JsonSerializerOptions()
+            Mapper mapper = new Mapper(new MapperConfiguration((cfg) =>
             {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            };
+                cfg.CreateMap<ComputerSnake, Computer>()
+                .ForMember(destination => destination.ComputerId, options =>
+                    options.MapFrom(source => source.computer_id))
+                .ForMember(destination => destination.CPUCores, options =>
+                    options.MapFrom(source => source.cpu_cores))
+                .ForMember(destination => destination.HasLTE, options =>
+                    options.MapFrom(source => source.has_lte))
+                .ForMember(destination => destination.HasWifi, options =>
+                    options.MapFrom(source => source.has_wifi))
+                .ForMember(destination => destination.Motherboard, options =>
+                    options.MapFrom(source => source.motherboard))
+                .ForMember(destination => destination.VideoCard, options =>
+                    options.MapFrom(source => source.video_card))
+                .ForMember(destination => destination.ReleaseDate, options =>
+                    options.MapFrom(source => source.release_date))
+                .ForMember(destination => destination.Price, options =>
+                    options.MapFrom(source => source.price));
+            }));
 
-            IEnumerable<Computer>? computersNewtonsoft = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson); // tworzenie listy z pliku json z paczka Newtonsoft.Json
-            IEnumerable<Computer>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options); // tworzenie listy z pliku json korzystając z System.Text.Json, musimy uzyć options zeby bylo case sensitive
-
-            if (computersNewtonsoft != null)
+            IEnumerable<Computer>? computersSystemJsonPropertyMapping = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson); // tworzenie listy z pliku json korzystając z System.Text.Json, musimy uzyć options zeby bylo case sensitive
+            if (computersSystemJsonPropertyMapping != null)
             {
-                foreach (Computer computer in computersNewtonsoft)
+                Console.WriteLine(computersSystemJsonPropertyMapping.Count());
+                foreach (Computer computer in computersSystemJsonPropertyMapping)
                 {
-                    //Console.WriteLine(computer.Motherboard);
-                    string sql = @"INSERT INTO TutorialAppSchema.Computer (
-                Motherboard,
-                HasWifi,
-                HasLTE,
-                ReleaseDate,
-                Price,
-                VideoCard
-            ) VALUES ('" + EscapeSingleQuote(computer.Motherboard)
-               + "','" + computer.HasWifi
-               + "','" + computer.HasLTE
-               + "','" + computer.ReleaseDate?.ToString("yyyy-MM-dd")
-               + "','" + computer.Price.ToString("0.00", CultureInfo.InvariantCulture)
-               + "','" + EscapeSingleQuote(computer.VideoCard)
-            + "')";
-
-                    dapper.ExecuteSql(sql);
+                    Console.WriteLine(computer.Motherboard);
                 }
             }
 
-            JsonSerializerSettings settings = new JsonSerializerSettings() // w Newtonsoft jak chcemy serializowac do pierwotnej formy musimy uzyc tych settingsow
-            {
-                ContractResolver = new CamelCasePropertyNamesContractResolver()
-            };
 
-            string computersCopyNewtonsoft = JsonConvert.SerializeObject(computersNewtonsoft, settings);
-            File.WriteAllText("computersCopyNewtonsoft.txt", "\n" + computersCopyNewtonsoft + "\n");
-            
-            string computersCopySystem = System.Text.Json.JsonSerializer.Serialize(computersSystem, options);
-            File.WriteAllText("computersCopySystem.txt", "\n" + computersCopySystem + "\n");
+            IEnumerable<ComputerSnake>? computersSystemAutoMapper = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<ComputerSnake>>(computersJson); // tworzenie listy z pliku json korzystając z System.Text.Json, musimy uzyć options zeby bylo case sensitive
+
+            if (computersSystemAutoMapper != null)
+            {
+                IEnumerable<Computer> computerResult = mapper.Map<IEnumerable<Computer>>(computersSystemAutoMapper);
+                Console.WriteLine(computerResult.Count());
+                foreach (Computer computer in computerResult)
+                {
+                    Console.WriteLine(computer.ReleaseDate);
+                }
+            }
+
+            //Console.WriteLine(computersJson);
+
+            //JsonSerializerOptions options = new JsonSerializerOptions()
+            //{
+            //    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            //};
+
+            //IEnumerable<Computer>? computersNewtonsoft = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson); // tworzenie listy z pliku json z paczka Newtonsoft.Json
+            //IEnumerable<Computer>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options); // tworzenie listy z pliku json korzystając z System.Text.Json, musimy uzyć options zeby bylo case sensitive
+
+            //if (computersNewtonsoft != null)
+            //{
+            //    foreach (Computer computer in computersNewtonsoft)
+            //    {
+            //        //Console.WriteLine(computer.Motherboard);
+            //        string sql = @"INSERT INTO TutorialAppSchema.Computer (
+            //    Motherboard,
+            //    HasWifi,
+            //    HasLTE,
+            //    ReleaseDate,
+            //    Price,
+            //    VideoCard
+            //) VALUES ('" + EscapeSingleQuote(computer.Motherboard)
+            //   + "','" + computer.HasWifi
+            //   + "','" + computer.HasLTE
+            //   + "','" + computer.ReleaseDate?.ToString("yyyy-MM-dd")
+            //   + "','" + computer.Price.ToString("0.00", CultureInfo.InvariantCulture)
+            //   + "','" + EscapeSingleQuote(computer.VideoCard)
+            //+ "')";
+
+            //        dapper.ExecuteSql(sql);
+            //    }
+            //}
+
+            //JsonSerializerSettings settings = new JsonSerializerSettings() // w Newtonsoft jak chcemy serializowac do pierwotnej formy musimy uzyc tych settingsow
+            //{
+            //    ContractResolver = new CamelCasePropertyNamesContractResolver()
+            //};
+
+            //string computersCopyNewtonsoft = JsonConvert.SerializeObject(computersNewtonsoft, settings);
+            //File.WriteAllText("computersCopyNewtonsoft.txt", "\n" + computersCopyNewtonsoft + "\n");
+
+            //string computersCopySystem = System.Text.Json.JsonSerializer.Serialize(computersSystem, options);
+            //File.WriteAllText("computersCopySystem.txt", "\n" + computersCopySystem + "\n");
         }
 
         static string EscapeSingleQuote(string input)
